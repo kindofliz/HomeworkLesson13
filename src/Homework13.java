afeb469088767fb4bd8a6629b9d23495603a5291
kindofliz <simanovicha@gmail.com>
2021-07-28 21:25:21 +0300

    added user inout examples and regex examples during class

--- src/Homework13.java
+++ src/Homework13.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Homework13 {
     public static void main(String[] args) {
 
@@ -80,6 +83,100 @@ public class Homework13 {
             System.out.println(tempVar);
 
 //        System.out.println("Numerical array printed out in a string representation: " + Arrays.toString(numCities));
+
+
+
+
+
+
+
+
+
+        //CLASS WORK - SEARCH MENU FOR THE ARRAY 28.08 //SECOND PART OF HOMEWORK
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        //CLASS TASK WITH USER INPUT - THE LAST PART
+        System.out.println("Please input the part of the name here: ");
+        String userInput = scanner.next();
+
+        for (String city: citiesArray) {
+            if (city.matches(".*"+userInput+".*")) {
+                System.out.println("This city matches with the RegEx: \"" + userInput + "\" | " + city);
+            }
+        }
+
+
+
+        //CLASS TASK WITH USER INPUT - THE LAST PART
+        //CASE INSENSITIVE METHOD with regex
+        //WE WILL LOWER THE CASE OF THE USER INPUT AND THE CITY
+        System.out.println("Please input the part of the name here: ");
+        String userInput2 = scanner.next();
+
+        for (String city: citiesArray) {
+            if (city.toLowerCase().matches(".*"+userInput2.toLowerCase()+".*")) {
+                System.out.println("This city matches with the RegEx: \"" + userInput + "\" | " + city);
+            }
+        }
+
+
+        //CASE INSENSITIVE METHOD without regex - ONLY FOR THE FIRST LETTER (CHAR)
+        //WE WILL LOWER THE CASE OF THE USER INOUT AND THE CITY
+        System.out.println("Please input the part of the name here: ");
+        String userInput3 = scanner.next();
+
+        for (String city: citiesArray) {
+            if (city.toLowerCase().charAt(0) == userInput3.toLowerCase().charAt(0)) {
+                System.out.println("This city matches with the RegEx: \"" + userInput + "\" | " + city);
+            }
+        }
+
+
+
+
+
+
+
+        //REGEX EXAMPLES - CLASS
+        //A.*s -> starts with a ends with s
+        //^Starts with
+        //contains only letters a.....z (lower case only) -> [a-z]+
+        //lower and upper case letters -> [a-zA-Z]
+        //looking for a digit -> \\d (a single digit) SAME AS [0-9]
+
+        //VALIDATE YEAR RANGE: 1900 - 2100 -> [0-9]{4}
+        String validInput = "2020";
+        String invalidInput = "99999";
+
+        //Good but can be better! Look below!
+        //correct input (also works = ("\\d\\d\\d\\d")
+        if("2020".matches("[0-9]{4}")){
+            System.out.println("Valid input is a valid year!");
+        } else {
+            System.out.println("Valid input is not a valid year");
+        }
+
+
+        //BETTER WAY TO VALIDATE YEAR RANGE 1900 - 2100
+        //but this would also validate year 2999 for example..
+        if("2999".matches("[0-2][019][0-9][0-9]")){
+            System.out.println("Valid input is a valid year! Enhanced validation rules!");
+        } else {
+            System.out.println("Valid input is not a valid year");
+        }
+
+
+        //EVEN BETTER WAY TO VALIDATE YEAR RANGE 1900 - 2100
+        if("2099".matches("((19|20)\\d\\d|2100)")){
+            System.out.println("Valid input is a valid year! Fully working!");
+        } else {
+            System.out.println("Valid input is not a valid year. Fully working!");
+        }
+
+
+
 
 
 
