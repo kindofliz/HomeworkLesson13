6c521a60058aab1287fef6136d6495c1f41174a6
kindofliz <simanovicha@gmail.com>
2021-07-29 14:23:43 +0300

    added counters and else if statements if nothing matches the search parameters

--- src/Homework13.java
+++ src/Homework13.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Homework13 {
@@ -12,7 +11,6 @@ public class Homework13 {
         String[] citiesArray = {"Copenhagen", "Paris", "Tokyo", "Bristol", "Mumbai", "Delhi", "Riga", "Vienna", "Warsaw", "Hamburg", "Cesis"};
 
 
-
         //1. Find out how many values are starting with ‘C’;
         int counterC = 0;
         for (int i = 0; i < citiesArray.length; i++) {
@@ -21,8 +19,6 @@ public class Homework13 {
             }
         }
         System.out.println("Number of values starting with 'C': " + counterC);
-
-
 
 
         //2. Find out how many values are ending with “i”; EXAMPLE 1
@@ -35,8 +31,6 @@ public class Homework13 {
         System.out.println("Number of values ending with 'i': " + counterI);
 
 
-
-
         //3. Find out how many values are with length 5
         int counterFive = 0;
         for (int i = 0; i < citiesArray.length; i++) {
@@ -45,8 +39,6 @@ public class Homework13 {
             }
         }
         System.out.println("Number of values with the length of 5 letters: " + counterFive);
-
-
 
 
         //4. Find out how many values holds the character “e”
@@ -59,8 +51,6 @@ public class Homework13 {
         System.out.println("Number of values holding the char 'e': " + counterE);
 
 
-
-
         //5. Find out is there any element which holds the subString “en”
         int counterEn = 0;
         for (int i = 0; i < citiesArray.length; i++) {
@@ -71,12 +61,10 @@ public class Homework13 {
         System.out.println("Yes, there are " + counterEn + " elements that hold the subString 'en'.");
 
 
-
-
         //6. *Create numerical array and, using LOOP fill it with corresponding lengths of values from array with city names, like: Copenhagen = 10, Paris = 5...
         System.out.println("----------------------------------THE NUMERICAL ARRAY------------------------------------");
 
-        int[] numCities = {citiesArray[0].length(),citiesArray[1].length(),citiesArray[2].length(),citiesArray[3].length(),citiesArray[4].length(),citiesArray[5].length(),citiesArray[6].length(),citiesArray[7].length(),citiesArray[8].length(),citiesArray[9].length(),citiesArray[10].length()};
+        int[] numCities = {citiesArray[0].length(), citiesArray[1].length(), citiesArray[2].length(), citiesArray[3].length(), citiesArray[4].length(), citiesArray[5].length(), citiesArray[6].length(), citiesArray[7].length(), citiesArray[8].length(), citiesArray[9].length(), citiesArray[10].length()};
 
         //FILLING/PRINTING THE ARRAY with corresponding numbers, using FOR EACH loop.
         for (int tempVar : numCities)
@@ -85,12 +73,6 @@ public class Homework13 {
 //        System.out.println("Numerical array printed out in a string representation: " + Arrays.toString(numCities));
 
         System.out.println();
-
-
-
-
-
-
 
 
         //CLASS WORK - SEARCH MENU FOR THE ARRAY 28.08 //SECOND PART OF HOMEWORK
@@ -105,11 +87,17 @@ public class Homework13 {
         System.out.println("1. Please input the first letter of the city name: ");
         String userInputFirst = scanner.next();
 
-        for (String city: citiesArray) {
-            if (city.toLowerCase().matches("^"+userInputFirst.toLowerCase()+".*")) {
+        int counterFirst = 0;
+        for (String city : citiesArray) {
+            if (city.toLowerCase().matches("^" + userInputFirst.toLowerCase() + ".*")) {
                 System.out.println("This city name starts with the letter: \"" + userInputFirst + "\" | " + city);
+                counterFirst++;
+            } else if (counterFirst == 0) {
+                    System.out.println("No city name starts with this letter!");
+                    break;
+                }
             }
-        }
+
 
 //        //1. first letter of the name with charAT
 //
@@ -126,12 +114,16 @@ public class Homework13 {
         System.out.println("2. Please input the last letter of the city name: ");
         String userInputLast = scanner.next();
 
-        for (String city: citiesArray) {
-            if (city.toLowerCase().matches(".*"+userInputLast.toLowerCase()+"")) {
+        int counterLast = 0;
+        for (String city : citiesArray) {
+            if (city.toLowerCase().matches(".*" + userInputLast.toLowerCase() + "")) {
                 System.out.println("This city name ends with the letter: \"" + userInputLast + "\" | " + city);
+                counterLast++;
+            } else if (counterLast == 0) {
+                System.out.println("No city name ends with this letter!");
+                break;
             }
         }
-
 
 
         //3. length  of the name
@@ -140,12 +132,17 @@ public class Homework13 {
         System.out.println("3. Please input the length of the city name: ");
         int userInputLength = scanner.nextInt();
 
+        int counterLength = 0;
         for (int i = 0; i < citiesArray.length; i++) {
             if (citiesArray[i].length() == userInputLength) {
                 System.out.println("City name with the length of \"" + userInputLength + "\" letters is | " + citiesArray[i]);
+                counterLength++;
+            } else if (counterLength == 0) {
+                System.out.println("There is no city name with such length!");
+                break;
             }
-        }
 
+        }
 
 
         //4. finding a part of the name
@@ -154,12 +151,16 @@ public class Homework13 {
         System.out.println("4. Please input part of the city name here: ");
         String userInput = scanner.next();
 
-        for (String city: citiesArray) {
-            if (city.matches(".*"+userInput+".*")) {
+        int counterPart = 0;
+        for (String city : citiesArray) {
+            if (city.matches(".*" + userInput + ".*")) {
                 System.out.println("This city matches with the RegEx: \"" + userInput + "\" | " + city);
+                counterPart++;
+            } else if (counterPart == 0) {
+                System.out.println("There is no city name that consists of your input!");
+                break;
             }
         }
-
 
 
         //CASE INSENSITIVE METHOD with regex + MAKING IT CASE INSENSITIVE BY LOWERING USER INPUT AND CITY CASE
@@ -167,8 +168,8 @@ public class Homework13 {
         System.out.println("4.1 Please input part of the name here: ");
         String userInput2 = scanner.next();
 
-        for (String city: citiesArray) {
-            if (city.toLowerCase().matches(".*"+userInput2.toLowerCase()+".*")) {
+        for (String city : citiesArray) {
+            if (city.toLowerCase().matches(".*" + userInput2.toLowerCase() + ".*")) {
                 System.out.println("This city matches with the RegEx: \"" + userInput2 + "\" | " + city);
             }
         }
@@ -179,21 +180,16 @@ public class Homework13 {
         System.out.println("4.2 Please input part of the name here: ");
         String userInput3 = scanner.next();
 
-        for (String city: citiesArray) {
+        for (String city : citiesArray) {
             if (city.toLowerCase().charAt(0) == userInput3.toLowerCase().charAt(0)) {
                 System.out.println("This city matches with the RegEx: \"" + userInput3 + "\" | " + city);
             }
         }
 
 
-
-
         System.out.println();
         System.out.println("----------------------------END OF SEARCH MENU FOR THE ARRAY-------------------------------");
         System.out.println();
-
-
-
 
 
         //REGEX EXAMPLES - CLASS
@@ -208,7 +204,7 @@ public class Homework13 {
 
         //Good but can be better! Look below!
         //correct input (also works = ("\\d\\d\\d\\d")
-        if("2020".matches("[0-9]{4}")){
+        if ("2020".matches("[0-9]{4}")) {
             System.out.println("Valid input is a valid year!");
         } else {
             System.out.println("Valid input is not a valid year");
@@ -217,7 +213,7 @@ public class Homework13 {
 
         //BETTER WAY TO VALIDATE YEAR RANGE 1900 - 2100
         //but this would also validate year 2999 for example..
-        if("2999".matches("[0-2][019][0-9][0-9]")){
+        if ("2999".matches("[0-2][019][0-9][0-9]")) {
             System.out.println("Valid input is a valid year! Enhanced validation rules!");
         } else {
             System.out.println("Valid input is not a valid year");
@@ -225,7 +221,7 @@ public class Homework13 {
 
 
         //EVEN BETTER WAY TO VALIDATE YEAR RANGE 1900 - 2100
-        if("2099".matches("((19|20)\\d\\d|2100)")){
+        if ("2099".matches("((19|20)\\d\\d|2100)")) {
             System.out.println("Valid input is a valid year! Fully working!");
         } else {
             System.out.println("Valid input is not a valid year. Fully working!");
